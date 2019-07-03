88fb0d2134a684731eaeace19e4512d7b6705938
gribnoff <quinvex88@gmail.com>
2019-07-03 14:09:39 +0300

    3 задача
    методы с фруктами и обобщениями

---
+++ src/lesson_1/generics/fruits/Box.java
@@ -0,0 +1,43 @@
+package lesson_1.generics.fruits;
+
+import java.util.ArrayList;
+
+class Box<T extends Fruit> {
+    private ArrayList<T> content = new ArrayList<>();
+
+    @SuppressWarnings("WeakerAccess")
+    ArrayList<T> getContent() {
+        return content;
+    }
+
+    void put(T fruit){
+        content.add(fruit);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    float getWeight() {
+        float weight = 0;
+        for (Fruit fruit : content) {
+            weight += fruit.getWeight();
+        }
+
+        return weight;
+    }
+
+    boolean compare(Box box) {
+        return getWeight() == box.getWeight();
+    }
+
+    void shiftContent(Box<T> target) {
+        target.getContent().addAll(getContent());
+        getContent().clear();
+    }
+
+    @Override
+    public String toString() {
+        if (getContent().isEmpty())
+            return "Коробка пуста!";
+        else
+            return String.format("В коробке %s - %dшт. Вес: %.1f", getContent().get(0).toString(), getContent().size(), getWeight());
+    }
+}
