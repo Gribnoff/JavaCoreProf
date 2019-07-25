f445661b41b0e320c49c781eacec6f4f1a8833bb
gribnoff <quinvex88@gmail.com>
2019-07-26 00:58:29 +0300

    lesson 5 additional homework - the bay, ships loading

---
+++ src/lesson_5/the_bay/bay/Bay.java
@@ -0,0 +1,25 @@
+package lesson_5.the_bay.bay;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Bay {
+    private ArrayList<MapPoint> points;
+
+    public ArrayList<MapPoint> getPoints() {
+        return points;
+    }
+
+    public Bay(MapPoint... points) {
+        this.points = new ArrayList<>(Arrays.asList(points));
+    }
+
+    public boolean isJobDone() {
+        boolean result = true;
+        for (LoadPort port: LoadPort.getLoadPorts()) {
+            if (!port.isEmpty())
+                result = false;
+        }
+        return result;
+    }
+}
