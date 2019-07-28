4d71abf0ab374869360284b875c6b8183a4f389e
gribnoff <quinvex88@gmail.com>
2019-07-28 13:23:26 +0300

    lesson 6 chat start condition

---
+++ src/lesson_6/logsandtests/task1_chat_loggin/server/Main.java
@@ -0,0 +1,9 @@
+package lesson_6.logsandtests.task1_chat_loggin.server;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        new Server();
+    }
+}
